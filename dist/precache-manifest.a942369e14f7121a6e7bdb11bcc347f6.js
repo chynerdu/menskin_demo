af68652665c4c10980fbd76c55381f31ebaef807
Chindeu Uche <chinedu@crenettechlabs.com>
2020-11-19 11:42:50 +0100

    pushed changes

---
+++ dist/precache-manifest.a942369e14f7121a6e7bdb11bcc347f6.js
@@ -0,0 +1,302 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "669bc8e1497b2c0a3be0818147aae3f2",
+    "url": "/assets/css/bootstrap.min.css"
+  },
+  {
+    "revision": "7585ceade513062d30b4d6a64e968961",
+    "url": "/assets/css/custom.css"
+  },
+  {
+    "revision": "ba9b78d1cbd5908a02745253260e26d3",
+    "url": "/assets/css/fontawesome.min.css"
+  },
+  {
+    "revision": "ad1177488914b67f38c1b627667bf84d",
+    "url": "/assets/css/spacing.css"
+  },
+  {
+    "revision": "483b9900f694240fc8f7cb8f9c482438",
+    "url": "/assets/fonts/IBMPlexSans-Bold.eot"
+  },
+  {
+    "revision": "c197bd53158a27f5afc1fd70380f4a8e",
+    "url": "/assets/fonts/IBMPlexSans-Bold.woff"
+  },
+  {
+    "revision": "529c61130cb03a3a0561d92bcaa95363",
+    "url": "/assets/fonts/IBMPlexSans-Bold.woff2"
+  },
+  {
+    "revision": "6f594a017b93ce0c5fd50fe4b65d842e",
+    "url": "/assets/fonts/IBMPlexSans-ExtraLight.eot"
+  },
+  {
+    "revision": "5b75b2b333a4c7a7e596557146593512",
+    "url": "/assets/fonts/IBMPlexSans-ExtraLight.woff"
+  },
+  {
+    "revision": "34a39af0ab96806c58b1aa5e94f3b206",
+    "url": "/assets/fonts/IBMPlexSans-ExtraLight.woff2"
+  },
+  {
+    "revision": "2f399318f4c7c9ce2e01738ff083bea0",
+    "url": "/assets/fonts/IBMPlexSans-Light.eot"
+  },
+  {
+    "revision": "aea011a40106c2b778159a232ac49e3b",
+    "url": "/assets/fonts/IBMPlexSans-Light.woff"
+  },
+  {
+    "revision": "d5736c19ecfba4a6833a39b23c168bfa",
+    "url": "/assets/fonts/IBMPlexSans-Light.woff2"
+  },
+  {
+    "revision": "51655d4275d26c40eb6bd80bfc392ee4",
+    "url": "/assets/fonts/IBMPlexSans-Medium.eot"
+  },
+  {
+    "revision": "4af9af1e344af587d757e54d5d2ea41f",
+    "url": "/assets/fonts/IBMPlexSans-Medium.woff"
+  },
+  {
+    "revision": "95852229d616b6f65b67e63a02264700",
+    "url": "/assets/fonts/IBMPlexSans-Medium.woff2"
+  },
+  {
+    "revision": "05ddf2e0f8d5fa4155252a4fb8f527b5",
+    "url": "/assets/fonts/IBMPlexSans-SemiBold.eot"
+  },
+  {
+    "revision": "78774c4c6f2de63e71e5286b19d23479",
+    "url": "/assets/fonts/IBMPlexSans-SemiBold.woff"
+  },
+  {
+    "revision": "433586aca654336996b3c4624ca8383f",
+    "url": "/assets/fonts/IBMPlexSans-SemiBold.woff2"
+  },
+  {
+    "revision": "d076f34c49eeadc7512b31c4941678c9",
+    "url": "/assets/fonts/IBMPlexSans.eot"
+  },
+  {
+    "revision": "7a37320158494837cdf0d4796a0a7c7a",
+    "url": "/assets/fonts/IBMPlexSans.woff"
+  },
+  {
+    "revision": "f6cc751e965b2a66175c362280dd6372",
+    "url": "/assets/fonts/IBMPlexSans.woff2"
+  },
+  {
+    "revision": "4a1d8c27dac2c1bdee46fcfb5c80089b",
+    "url": "/assets/fonts/Roboto-Bold.eot"
+  },
+  {
+    "revision": "16d7bb99c6f81cacdd91cd92d8ddb545",
+    "url": "/assets/fonts/Roboto-Bold.ttf"
+  },
+  {
+    "revision": "08cb8f79715774f9a6285ee7db2919a3",
+    "url": "/assets/fonts/Roboto-Bold.woff"
+  },
+  {
+    "revision": "f3a02e2578bee50e620e515912278bc9",
+    "url": "/assets/fonts/Roboto-Bold.woff2"
+  },
+  {
+    "revision": "35d85034cc6efe254752721f40dae9f4",
+    "url": "/assets/fonts/Roboto-Light.eot"
+  },
+  {
+    "revision": "2382fa8a8afcdbe3124c840bd6ef7024",
+    "url": "/assets/fonts/Roboto-Light.ttf"
+  },
+  {
+    "revision": "10ad0f861c0c5807734017c341940649",
+    "url": "/assets/fonts/Roboto-Light.woff"
+  },
+  {
+    "revision": "ed4b08d2702fa26acc324ef1e89ae837",
+    "url": "/assets/fonts/Roboto-Light.woff2"
+  },
+  {
+    "revision": "02aa6e7b39da1ff77803f67452addc43",
+    "url": "/assets/fonts/Roboto-Medium.eot"
+  },
+  {
+    "revision": "b2bcaa52d04bde9a494fd954ef7e7e7b",
+    "url": "/assets/fonts/Roboto-Medium.ttf"
+  },
+  {
+    "revision": "9a3bf7acae14d9b5ed5a88458106b58b",
+    "url": "/assets/fonts/Roboto-Medium.woff"
+  },
+  {
+    "revision": "50d01d3e6c994995bcaf829e63d53d1a",
+    "url": "/assets/fonts/Roboto-Medium.woff2"
+  },
+  {
+    "revision": "b9077621ce786b55c176a61456bfc077",
+    "url": "/assets/fonts/Roboto-Regular.eot"
+  },
+  {
+    "revision": "4312f1fbdcf4d54af4506dabdce08010",
+    "url": "/assets/fonts/Roboto-Regular.ttf"
+  },
+  {
+    "revision": "94dac78eee406a8c8f0406b69b85ac2b",
+    "url": "/assets/fonts/Roboto-Regular.woff"
+  },
+  {
+    "revision": "9feb0110b6dff9ee2b9ebd17f7a1aee6",
+    "url": "/assets/fonts/Roboto-Regular.woff2"
+  },
+  {
+    "revision": "405e637dbe0d43a3340d33b1db9f680a",
+    "url": "/assets/fonts/cryptofont-webfont.eot"
+  },
+  {
+    "revision": "f8e7a93e8cc68cd23e5481775d767e8d",
+    "url": "/assets/fonts/cryptofont-webfont.svg"
+  },
+  {
+    "revision": "bda334582cf63e3bf621e6a710bd3fd9",
+    "url": "/assets/fonts/cryptofont-webfont.ttf"
+  },
+  {
+    "revision": "6ea44f1f105ad7c7d8de96ff0891e192",
+    "url": "/assets/fonts/cryptofont-webfont.woff"
+  },
+  {
+    "revision": "748ab466bee11e0b2132916def799916",
+    "url": "/assets/fonts/fa-brands-400.eot"
+  },
+  {
+    "revision": "b032e14eac87e3001396ff597e4ec15f",
+    "url": "/assets/fonts/fa-brands-400.svg"
+  },
+  {
+    "revision": "7febe26eeb4dd8e3a3c614a144d399fb",
+    "url": "/assets/fonts/fa-brands-400.ttf"
+  },
+  {
+    "revision": "2248542e1bbbd548a157e3e6ced054fc",
+    "url": "/assets/fonts/fa-brands-400.woff"
+  },
+  {
+    "revision": "3654744dc6d6c37c9b3582b57622df5e",
+    "url": "/assets/fonts/fa-brands-400.woff2"
+  },
+  {
+    "revision": "b58f468f84168d61e0ebc1e1f423587c",
+    "url": "/assets/fonts/fa-regular-400.eot"
+  },
+  {
+    "revision": "3929b3ef871fa90bbb4e77e005851e74",
+    "url": "/assets/fonts/fa-regular-400.svg"
+  },
+  {
+    "revision": "54f142e03adc6da499c2af4f54ab76fd",
+    "url": "/assets/fonts/fa-regular-400.ttf"
+  },
+  {
+    "revision": "f3dd4f397fbc5aaf831b6b0ba112d75c",
+    "url": "/assets/fonts/fa-regular-400.woff"
+  },
+  {
+    "revision": "33f727ccde4b05c0ed143c5cd78cda0c",
+    "url": "/assets/fonts/fa-regular-400.woff2"
+  },
+  {
+    "revision": "035a137af03db6f1af76a589da5bb865",
+    "url": "/assets/fonts/fa-solid-900.eot"
+  },
+  {
+    "revision": "9bbbee00f65769a64927764ef51af6d0",
+    "url": "/assets/fonts/fa-solid-900.svg"
+  },
+  {
+    "revision": "b6a14bb88dbc580e45034af297c8f605",
+    "url": "/assets/fonts/fa-solid-900.ttf"
+  },
+  {
+    "revision": "6661d6b3521b4c480ba759e4b9e480c1",
+    "url": "/assets/fonts/fa-solid-900.woff"
+  },
+  {
+    "revision": "8a8c0474283e0d9ef41743e5e486bf05",
+    "url": "/assets/fonts/fa-solid-900.woff2"
+  },
+  {
+    "revision": "fd8e7fe015512e855c22d78ce9c32441",
+    "url": "/assets/fonts/ikon.eot"
+  },
+  {
+    "revision": "9df02f8bd23b1bc4602f185b88409145",
+    "url": "/assets/fonts/ikon.svg"
+  },
+  {
+    "revision": "a827498d7dc8f149c5d38554a6911b55",
+    "url": "/assets/fonts/ikon.ttf"
+  },
+  {
+    "revision": "995c0a8428c1d952f774e473f241308f",
+    "url": "/assets/fonts/ikon.woff"
+  },
+  {
+    "revision": "2c454669bdf3aebf32a1bd8ac1e0d2d6",
+    "url": "/assets/fonts/themify.eot"
+  },
+  {
+    "revision": "9c8e96ecc7fa01e6ebcd196495ed2db5",
+    "url": "/assets/fonts/themify.svg"
+  },
+  {
+    "revision": "e23a7dcaefbde4e74e263247aa42ecd7",
+    "url": "/assets/fonts/themify.ttf"
+  },
+  {
+    "revision": "a1ecc3b826d01251edddf29c3e4e1e97",
+    "url": "/assets/fonts/themify.woff"
+  },
+  {
+    "revision": "6569ba1e61c3824de53fe8e2d38e245b",
+    "url": "/assets/js/bootstrap.min.js"
+  },
+  {
+    "revision": "8101d596b2b8fa35fe3a634ea342d7c3",
+    "url": "/assets/js/jquery.js"
+  },
+  {
+    "revision": "cf6ae42381a40a3e89e6",
+    "url": "/css/app.d0b6b759.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "cd12afb07a25c339b5bf45a4690d1a8d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7a6d968a7bf1a10cc3ad",
+    "url": "/js/about.9fecafe8.js"
+  },
+  {
+    "revision": "cf6ae42381a40a3e89e6",
+    "url": "/js/app.d0bf2ffc.js"
+  },
+  {
+    "revision": "9cfe4d7914e0b9ef2bd2",
+    "url": "/js/chunk-vendors.5edc5583.js"
+  },
+  {
+    "revision": "01d83226ffe2b8b5d611f0568c10b0cd",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
